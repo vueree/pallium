241c08af6de57616ef71bdd621e7b13f62c49c8f
weelfis <weelfis@192.168.1.11.non-exists.ptr.local>
2024-12-06 13:05:37 +0700

    pull

--- nginx.conf.c
+++ nginx.conf.c
@@ -4,7 +4,28 @@ http {
         server_name pallium.onrender.com;
 
         location / {
-            try_files $uri $uri/ /index.html;
+            root /path/to/client;
+            index index.html;
+        }
+
+        location /api {
+            proxy_pass https://api-pallium.onrender.com;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection 'upgrade';
+            proxy_set_header Host $host;
+            proxy_cache_bypass $http_upgrade;
+            proxy_ssl_verify on;
+        }
+
+        location /register {
+            proxy_pass https://pallium.onrender.com/register;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection 'upgrade';
+            proxy_set_header Host $host;
+            proxy_cache_bypass $http_upgrade;
+            proxy_ssl_verify on;
         }
 
         error_page 500 502 503 504 /50x.html;
